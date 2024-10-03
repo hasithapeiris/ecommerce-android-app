829350a9a095306b74a5b95ff89be6edb520dcb2
Hasitha <hasithapeiris.me@gmail.com>
2024-10-03 11:21:08 +0530

    feat: create cart model

---
+++ app/src/main/java/com/example/ecommerceapp/models/CartModel.kt
@@ -0,0 +1,10 @@
+package com.example.ecommerceapp.models
+
+data class CartModel(
+    val pid: String? = null,
+    val uid: String? = null,
+    val imageUrl: String? = null,
+    val name: String? = null,
+    val price: String? = null,
+    val quantity: Int? = null
+)
