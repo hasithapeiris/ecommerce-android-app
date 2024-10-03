01f0ace995b028185bff20a62488de433376c15c
Hasitha <hasithapeiris.me@gmail.com>
2024-10-03 11:12:08 +0530

    feat: create item model

--- app/src/main/java/com/example/ecommerceapp/models/ItemModel.kt
+++ app/src/main/java/com/example/ecommerceapp/models/ItemModel.kt
@@ -1,4 +1,10 @@
 package com.example.ecommerceapp.models
 
-class ItemModel {
-}
+data class ItemModel(
+    val brand: String? = null,
+    val description: String? = null,
+    val id:String? = null ,
+    val imageUrl: String? = null,
+    val name: String? = null,
+    val price: String? = null,
+)
--- app/src/main/java/com/example/ecommerceapp/models/OrderModel.kt
+++
@@ -1,4 +0,0 @@
-package com.example.ecommerceapp.models
-
-class OrderModel {
-}
