cfb50926aa04887feaf86d6b912bd7a9846794da
Hasitha <hasithapeiris.me@gmail.com>
2024-10-03 12:38:15 +0530

    feat: implement cart adapter

--- app/src/main/java/com/example/ecommerceapp/models/CartModel.kt
+++ app/src/main/java/com/example/ecommerceapp/models/CartModel.kt
@@ -6,5 +6,6 @@ data class CartModel(
     val imageUrl: String? = null,
     val name: String? = null,
     val price: String? = null,
+    val size: String? = null,
     val quantity: Int? = null
 )
