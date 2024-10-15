5cd9ba654c731110bf3f1c89beafadb84aa4a69b
Baddewithana P <99246737+IT21247804@users.noreply.github.com>
2024-10-15 14:28:30 +0530

    delete review completed with small error

--- app/src/main/java/com/example/ecommerceapp/api/UpdateProfileApi.kt
+++ app/src/main/java/com/example/ecommerceapp/api/UpdateProfileApi.kt
@@ -10,11 +10,12 @@ import com.example.ecommerceapp.models.UpdateProfileResponse
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Header
+import retrofit2.http.POST
 import retrofit2.http.PUT
 
 interface UpdateProfileApi {
     //function to handle update profile api request in retrofit level
-    @PUT("/api/updateprofile")
+    @POST("/api/customer/users")
     fun updateProfile(
         @Header("Authorization") token: String,
         @Body updateProfileRequest: UpdateProfileRequest
